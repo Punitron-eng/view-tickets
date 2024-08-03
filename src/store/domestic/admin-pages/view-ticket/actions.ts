f2e2b4075d677224a43021d0615527238dd83dfc
Rajveer-ITL <rajveer@depasserinfotech.in>
2024-08-03 16:45:26 +0530

    small chages

--- src/store/domestic/admin-pages/view-ticket/actions.ts
+++ src/store/domestic/admin-pages/view-ticket/actions.ts
@@ -96,8 +96,6 @@ const createActions = () => ({
 
     // For datatable columns
     async [NEWVIEWTICKET.ACTIONS.GETDEFAULTCOLUMN](context: any, payload: any) {
-        console.log('ticketDepartmentId =>', context.getters.getTicketDepartmentId);
-
         payload.payload.ticket_department = context.getters.getTicketDepartmentId;
         const defaultColumn = await getDefaultColumnData(payload);
         context.commit(NEWVIEWTICKET.MUTATIONS.SETDEFAULTCOLUMN, defaultColumn);
