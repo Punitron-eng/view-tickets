4581f9830eeccf291fb87d3aafa5fb986e29147b
Rajveer-ITL <rajveer@depasserinfotech.in>
2024-08-16 01:23:19 +0530

    changes added

--- src/views/domestic/admin-pages/view-ticket/components/ChatModalHeader.vue
+++ src/views/domestic/admin-pages/view-ticket/components/ChatModalHeader.vue
@@ -165,7 +165,7 @@ const confirmPendingFnc = async () => {
                 </div>
             </div>
         </div>
-        <div class="flex w-full justify-end items-center gap-2 header-right-side">
+        <div class="flex w-full justify-end items-center gap-2 mobile-chat-modal-header">
             <div v-if="isLoading" class="flex justify-between items-center gap-2">
                 <SkeletonView width="100px" height="16px" />
                 <SkeletonView width="80px" height="32px" />
@@ -213,7 +213,7 @@ const confirmPendingFnc = async () => {
     @apply font-interSemiBold;
 }
 
-.header-right-side {
+.mobile-chat-modal-header {
     @media screen and (max-width: 767px) {
         width: 85% !important;
     }
