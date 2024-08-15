d2eeed57eb1dcdc0a7c0b591266737d9ded99a76
Rajveer-ITL <rajveer@depasserinfotech.in>
2024-08-16 01:56:33 +0530

    chnages added

--- src/views/domestic/admin-pages/view-ticket/viewTicket.vue
+++ src/views/domestic/admin-pages/view-ticket/viewTicket.vue
@@ -331,7 +331,7 @@ onMounted(() => {
         <div class="page-header-section mb-3">
             <DTPageHeader :headerText="dataVariables.selectedTabHeading">
                 <template #right-side>
-                    <div class="hidden lg:flex lg:items-center tk-create-btn">
+                    <div class="flex items-center tk-create-btn">
                         <HeaderRightSide />
                     </div>
                 </template>
